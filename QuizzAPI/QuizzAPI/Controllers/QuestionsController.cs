423e3e00f6bbb9c1c8de8e3c30ac2bbfc6111d6b
jivkopiskov <jivkopiskov@gmail.com>
2021-02-27 00:48:31 +0200

    Add questions controller and fix routing

---
+++ QuizzAPI/QuizzAPI/Controllers/QuestionsController.cs
@@ -0,0 +1,30 @@
+﻿namespace QuizzAPI.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using QuizzAPI.Data;
+    using QuizzAPI.Data.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    [Route("api/questions/{id:int?}")]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly QuizDbContext context;
+
+        public QuestionsController(QuizDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Question> GetByCategory(int id = 0)
+        {
+            var rng = new Random();
+            return this.context.Questions.Where(x => x.CategoryId == id).OrderBy(x => Guid.NewGuid()).ToList();
+        }
+    }
+}
--- QuizzAPI/QuizzAPI/Startup.cs
+++ QuizzAPI/QuizzAPI/Startup.cs
@@ -58,6 +58,9 @@ namespace QuizzAPI
             {
                 endpoints.MapControllers();
             });
+
+
+
         }
     }
 }
